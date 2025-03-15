2a19fb5c8066cfb1f74e7835584a97cdea122c6f
Jesus Laso <jesuslaso94@gmail.com>
2025-03-15 21:05:53 +0100

    Primera version del header Spa & Beauty

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/spa_beauty/', // Replace with your repository name
+    build: {
+        outDir: 'dist',
+    },
+});
